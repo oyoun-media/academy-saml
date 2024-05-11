7caee95227d6111803d14ce8b702f03a1f02a9cd
DASH <107540988+Oyoun-Media-DASH@users.noreply.github.com>
2024-05-11 04:15:46 +0300

    Update metadata.blade.php

--- resources/views/metadata.blade.php
+++ resources/views/metadata.blade.php
@@ -1,5 +1,5 @@
 <?xml version="1.0"?>
-<md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" validUntil="2027-01-20T19:04:25Z" cacheDuration="PT1485371065S" entityID="{{ config('samlidp.issuer_uri') }}">
+<md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" validUntil="2027-01-20T19:04:25Z" cacheDuration="PT1485371065S" entityID="{{ rtrim(config('app.url'), '/') . '/' . ltrim(config('samlidp.issuer_uri'), '/') }}">
   <md:IDPSSODescriptor WantAuthnRequestsSigned="false" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
     <md:KeyDescriptor use="signing">
       <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
